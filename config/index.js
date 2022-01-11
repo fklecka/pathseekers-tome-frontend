05c3ef8d1e6093dc635ee529685e1f1acd99d6c9
Florian Klecka <office@florianklecka.at>
2022-01-11 22:31:13 +0100

    serverurl fix

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ import AuthService from "../services/AuthService";
 
 const config = {
   apiUrl: "https://pathseekers-tome-api.herokuapp.com/api",
-  serverUrl: "https://pathseekers-tome-api.herokuapp.com/",
+  serverUrl: "https://pathseekers-tome-api.herokuapp.com",
   headers: {
     headers: {
       Authorization: AuthService.getFullToken(),
