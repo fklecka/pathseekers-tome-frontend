21702df710dea0781445ce7e157d27f098017c38
Florian Klecka <office@florianklecka.at>
2022-01-11 22:52:39 +0100

    url fix

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ import AuthService from "../services/AuthService";
 
 const config = {
   apiUrl: "https://angry-dubinsky-8666f4.netlify.app/api",
-  serverUrl: "https://angry-dubinsky-8666f4.netlify.app/",
+  serverUrl: "https://angry-dubinsky-8666f4.netlify.app",
   headers: {
     headers: {
       Authorization: AuthService.getFullToken(),
